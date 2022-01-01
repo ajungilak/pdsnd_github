c098df205ad2689277e2c4e5724e345024489a69
Alexander Jung <a.jung3045@gmail.com>
2022-01-01 17:17:08 +0100

    Rename a function more precise

--- .py/bikeshare.py
+++ .py/bikeshare.py
@@ -55,7 +55,7 @@ def get_filters():
     return city, month, day
 
 
-def load_data(city, month, day):
+def load_Data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
     Args:
@@ -212,7 +212,7 @@ def display_raw_data(df):
 def main():
     while True:
         city, month, day = get_filters()
-        df = load_data(city, month, day)
+        df = load_Data(city, month, day)
 
         time_stats(df)
         station_stats(df)
